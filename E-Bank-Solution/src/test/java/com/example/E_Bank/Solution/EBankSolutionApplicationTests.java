29da035dbb205572911411ad7ae355ca3ba0a91f
ER-RAJY <noureddineerajy@gmail.com>
2024-07-07 12:56:13 +0100

    ajouter model de class user et copmte avec interfaces repository

---
+++ E-Bank-Solution/src/test/java/com/example/E_Bank/Solution/EBankSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_Bank.Solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
